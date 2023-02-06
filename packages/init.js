4ee555d14870c7f39ec8ebc01b08c75ee2bae462
Keval Bhogayata <keval@middleware.io>
2023-02-06 13:50:10 +0530

    added log exporter

--- packages/init.js
+++ packages/init.js
@@ -2,6 +2,7 @@ let config;
 import "opentelemetry-sdk-workers/performance";
 import { WorkersSDK } from "opentelemetry-sdk-workers";
 import { OTLPProtoTraceExporter } from "opentelemetry-sdk-workers/exporters/OTLPProtoTraceExporter";
+import { OTLPProtoLogExporter } from "opentelemetry-sdk-workers/exporters/OTLPProtoLogExporter";
 import { Resource } from '@opentelemetry/resources';
 
 module.exports.init = (newConfig = {}) => {
@@ -23,6 +24,9 @@ module.exports.track = (request,ctx) => {
     })
     const sdk = new WorkersSDK(request, ctx, {
         traceExporter: new OTLPProtoTraceExporter({
+            url: config.target
+        }),
+        logExporter: new OTLPProtoLogExporter({
             url: config.target
         }),
         resource
